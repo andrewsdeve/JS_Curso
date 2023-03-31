ec57f96cec8d7ff7079a5ab57bff6626b41f81c6
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-03-30 19:57:29 -0500

    Operadores_JS

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+// Codigo que utilice booleans
+
+const autenticado = true;
+if(autenticado){
+    console.log('puedes ver netflix')
+}else{
+    console.log('No puede ver netflix')
+}
+
+// Operador ternario
+
+console.log(autenticado ? 'Si esta autenticado' : 'No esta autenticado');
