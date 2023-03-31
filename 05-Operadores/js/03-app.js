a7a4e7f3a680b54d1c1a082bbb54227e687a893a
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-03-30 19:45:49 -0500

    Operadores_JS

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,15 @@
+// Mull & Undefined
+
+let numero;
+console.log(numero);
+console.log(typeof(numero));
+
+// Null
+
+let numero2 = null;
+console.log(typeof(numero2))
+console.log(numero2);
+
+console.log(numero === numero2);
+
+
