a7bd790d18db870cc9235308853db03423c91186
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-03-31 20:33:32 -0500

    Metodos_I

---
+++ 08-Arrays/js/04-app.js
@@ -0,0 +1,9 @@
+const meses = ['Enero','Febrero','Marzo','Abril','Mayo'];
+
+
+meses[0] = 'Nuevo Mes';
+meses[10] = 'Ultimo Mes' // Son metodo que funcionan pero no son practicos
+
+
+console.table(meses);
+
