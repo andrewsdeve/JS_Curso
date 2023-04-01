ba739a7eae50808078c2324ea573386e993212b4
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-03-31 20:10:44 -0500

    Froma_de_Crear_Arreglos

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,10 @@
+const numeros = [10,20,30];
+
+const meses = new Array('Enero','Febrero','Marzo'); // Forma menos utilizada
+console.log(numeros);
+console.log(meses);
+
+// Arreglo que contiene todo tipo de datos
+
+const deTodo = ['Hola',10, true, null, undefined, 'Soy Desarrollador', 10.25];
+console.log(deTodo);
