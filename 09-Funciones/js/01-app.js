cebc4906010838e1ff539f3be827cc79fcedb244
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-04-04 17:22:39 -0500

    Declaracion_funciones

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,14 @@
+// Declacracion de Funcion
+
+function sumar (){
+    console.log(2 + 2);
+}
+sumar();
+
+// expresion de funcion - Fuction Xpression
+
+const sumar2 = function (){
+    console.log( 3 + 7);
+}
+sumar2();
+
